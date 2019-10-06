538462fecdd7e6fc410c90d53b3c776f63ae3994
Daphne <daphne.augier@gmail.com>
2019-10-06 19:36:02 -0400

    Rename of files, footer fixed

---
+++ footer.php
@@ -0,0 +1,67 @@
+?>
+<!-- Footer -->
+
+    <footer id="footer">
+            <section>
+                <form class="contact-form" method="post" action="contactform.php">
+                    <div class="fields">
+                        <div class="field">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                        <div class="field">
+                            <label for="email">Email</label>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                        <div class="field">
+                            <label for="message">Message</label>
+                            <textarea name="message" id="message" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <ul class="actions">
+                        <li><input type="submit" value="Send Message" /></li>
+                    </ul>
+                </form>
+            </section>
+            <section class="split contact">
+                <section class="alt">
+                    <h3>Address</h3>
+                    <p>201-109 Rue Charlotte<br />
+                        Montreal, QC  H2X 1M2 </p>
+                    </p>
+                </section>
+                <section>
+                    <h3>Phone</h3>
+                    <p><a href="#">+1 (514) 812-2857‬</a></p>
+                </section>
+                <section>
+                    <h3>Email</h3>
+                    <p><a href="#">soccer4other@gmail.com</a></p>
+                </section>
+                <section>
+                    <h3>Social</h3>
+                    <ul class="icons alt">
+                            <li><a href="https://www.instagram.com/soccer4other/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                            <li><a href="https://www.facebook.com/Soccer4Other/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                    </ul>
+                </section>
+            </section>
+        </footer>
+
+    <!-- Copyright -->
+        <div id="copyright">
+            <ul><li>&copy; Soccer4Other</li><li>Design: <a href="https://github.com/daphneaugier">Daphné Augier | HTML5 UP</a></li></ul>
+        </div>
+
+</div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+<?php
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<!--
+	Massively by | HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Soccer4Other</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="shortcut icon" type="image/png" href="images/favicon.png">
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<div id="intro">
+						<h1>Soccer4Other</h1>
+						<p>Playing soccer whilst contributing to social change is possible</p>
+						<ul class="actions">
+							<li><a href="#nav" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
+
+				<!-- Header -->
+					<header id="header">
+						<a></a>
+					</header>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="links">
+							<li class="active"><a href="index.html">About Us</a></li>
+							<li><a href="pastevents.html">Past events</a></li>
+							<li><a href="registration.html">Event Registration</a></li>
+							<!-- <li><a href="findateam.html">Find a team</a></li> -->
+							<!--	<li><a href="forum.html">Forum</a></li> -->
+						</ul>
+						<ul class="icons">
+							<li><a href="https://www.instagram.com/soccer4other/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="https://www.facebook.com/Soccer4Other/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+						</ul>
+					</nav>
+
+							<!-- Main -->
+							<div id="main">
+
+									<!-- Post -->
+										<section class="post">
+											<header class="major">
+												<span class="date">Who are we?</span>
+												<p><b>Soccer4Other</b> is a social start-up that organizes soccer tournaments to support education through sport in 
+													underprivileged areas of the world. We have previously organized tournaments in Montreal, Mexico and France.
+													To foster this social change, we dedicate all of our profits to sport-oriented local charities.</p>
+													Offering hope and opportunity for a better future is what drives our organization.
+											</header>
+											<div class="image main"><img src="images/valbonne.jpg" alt="" /></div> 
+										<!--	<h3>VISION</h3>
+											<p>Use sports values as drivers of social change. </p>
+											<h3>MISSION</h3>
+											<p>Offering hope and opportunity for a better future is what drives our organization.</p> -->
+										
+
+										</section>
+			
+                                </div>
+
+<!--FOOTER-->
+<?php include('footer.php'); ?>                                
+
+	</body>
+</html>
