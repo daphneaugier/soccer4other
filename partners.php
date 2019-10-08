3625e523b6b286edffd07139c610ba0f63f6b3d2
Daphne <daphne.augier@gmail.com>
2019-10-07 21:20:28 -0400

    added partner component xoxo

---
+++ partners.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<!--
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Soccer4Other</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="shortcut icon" type="image/png" href="images/favicon.png">
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+
+				<!-- Header -->
+				<header id="header">
+						<a></a>
+					</header>
+
+				<!-- Nav -->
+				<?php require('navbar.php'); ?>   
+
+						<!-- Main -->
+						<div id="main">
+		
+								<!-- Posts -->
+									<section class="posts">
+										<article>
+											<header>
+												<span class="date"></span>
+												<h2><a href="#">District 5</a></h2>
+											</header>
+											<img class="image partner" src="images/district5.png" alt="" />
+											<p></p>
+											<ul class="actions special">
+											<li><a href="https://www.district5soccer.com/fr/index.html" class="button">Go to website</a></li> 
+											</ul> 
+										</article>
+										<article>
+											<header>
+												<span class="date"></span>
+												<h2><a>Pour 3 points</a></h2>
+											</header>
+											<a class="image partner"><img src="images/pour3points.jpg" alt="" /></a>
+											<p></p>
+											<ul class="actions special">
+										    <li><a href="https://pour3points.ca/" class="button">Go to website </a></li> 
+											</ul> 
+										</article>
+
+									</section>
+								</div>
+<!-- Footer -->
+<?php include('footer.php'); ?>   
+
+	</body>
+</html>
