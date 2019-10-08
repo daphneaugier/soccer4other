73c98a9311e55a6c65030e5b50a12bc6e34b154f
Daphne <daphne.augier@gmail.com>
2019-10-07 20:57:21 -0400

    navbar outside

--- index.php
+++ index.php
@@ -33,19 +33,7 @@
 					</header>
 
 				<!-- Nav -->
-					<nav id="nav">
-						<ul class="links">
-							<li class="active"><a href="index.php">About Us</a></li>
-							<li><a href="pastevents.php">Past events</a></li>
-							<li><a href="registration.php">Event Registration</a></li>
-							<!-- <li><a href="findateam.html">Find a team</a></li> -->
-							<!--	<li><a href="forum.html">Forum</a></li> -->
-						</ul>
-						<ul class="icons">
-							<li><a href="https://www.instagram.com/soccer4other/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-							<li><a href="https://www.facebook.com/Soccer4Other/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-						</ul>
-					</nav>
+				<?php require('navbar.php'); ?>   
 
 							<!-- Main -->
 							<div id="main">
